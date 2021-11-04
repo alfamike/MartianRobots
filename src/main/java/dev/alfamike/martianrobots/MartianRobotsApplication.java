a4323604fa7b27a3c9ef73445d895177346e31fb
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2021-11-04 20:32:34 +0100

    Initial commit

---
+++ src/main/java/dev/alfamike/martianrobots/MartianRobotsApplication.java
@@ -0,0 +1,13 @@
+package dev.alfamike.martianrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartianRobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartianRobotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alfamike/martianrobots/MartianRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alfamike.martianrobots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartianRobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
