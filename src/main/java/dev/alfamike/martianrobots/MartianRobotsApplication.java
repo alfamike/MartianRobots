ffd8be32361ea8d5c0e77d461b4bf9a339ddc3c3
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2021-11-07 16:04:59 +0100

    Update MartianRobotsApplication.java

--- src/main/java/dev/alfamike/martianrobots/MartianRobotsApplication.java
+++ src/main/java/dev/alfamike/martianrobots/MartianRobotsApplication.java
@@ -2,13 +2,15 @@ package dev.alfamike.martianrobots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "dev.alfamike.martianrobots.repository")
+@EntityScan(basePackages = "dev.alfamike.martianrobots.model")
 public class MartianRobotsApplication {
 
 	public static void main(String[] args) {
