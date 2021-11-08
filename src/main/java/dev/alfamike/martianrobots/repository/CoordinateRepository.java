01faf9108e2c3838445cff00241f578d9fe4fc9d
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2021-11-08 22:35:46 +0100

    Delete CoordinateRepository.java

--- src/main/java/dev/alfamike/martianrobots/repository/CoordinateRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.alfamike.martianrobots.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.alfamike.martianrobots.model.Coordinate;
-
-public interface CoordinateRepository extends JpaRepository<Coordinate, Long> {
-
-}
