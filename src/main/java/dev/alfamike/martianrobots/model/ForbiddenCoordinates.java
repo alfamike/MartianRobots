8ab6afd02ab3d42fcfde5e22ed37a6d3ab5c303d
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2021-11-10 21:12:18 +0100

    Update ForbiddenCoordinates.java

--- src/main/java/dev/alfamike/martianrobots/model/ForbiddenCoordinates.java
+++ src/main/java/dev/alfamike/martianrobots/model/ForbiddenCoordinates.java
@@ -1,14 +1,21 @@
 package dev.alfamike.martianrobots.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class ForbiddenCoordinates {
+public class ForbiddenCoordinates implements Serializable{
 	
-    public ForbiddenCoordinates(int xAxis, int yAxis) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ForbiddenCoordinates(int xAxis, int yAxis) {
 		super();
 		this.XAxis = xAxis;
 		this.YAxis = yAxis;
